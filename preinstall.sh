16e87a67ba33cb9930999154868564855184b6ea
moay <mv@moay.de>
2020-01-03 23:42:20 +0100

    Fixes setup

--- preinstall.sh
+++ preinstall.sh
@@ -7,11 +7,12 @@ TARGETDIR=$PDATA/vendor
 echo "<INFO> Preparing vendor libraries"
 php composer.phar install --no-interaction --no-dev
 
-echo "<INFO> Copying vendor libraries to $TARGETDIR"
-rm -rf $TARGETDIR
-mv ./data/vendor/* $TARGETDIR
+#echo "<INFO> Copying vendor libraries to $TARGETDIR"
+#rm -rf $TARGETDIR
+#mv ./data/vendor/* $TARGETDIR
+#rm -rf ./data/vendor
 
-echo "<SUCCESS> Done installing vendor libraries"
+echo "<OK> Done installing vendor libraries"
 
 # Exit with Status 0
 exit 0
