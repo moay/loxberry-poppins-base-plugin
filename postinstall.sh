e89b0cba4bae110755cc06f265a36716b4ece39e
moay <mv@moay.de>
2020-01-03 23:25:46 +0100

    Updates setup routine

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@ echo "<INFO> Installing vendor libraries"
 php composer.phar install --no-interaction --no-dev
 
 echo "<INFO> Copying vendor libraries to data folder"
-cp -r ./data/vendor $LBPDATA/$PDIR/vendor
+cp -r ./data/vendor $LBPDATA/$PDIR/$PSHNAME/vendor
 
 # Exit with Status 0
 exit 0
