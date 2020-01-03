5cbd6917bcd88be312021a013f3e20028146293c
moay <mv@moay.de>
2020-01-03 23:35:42 +0100

    Fixes setup

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,13 @@
 echo "<INFO> Installing vendor libraries"
 php composer.phar install --no-interaction --no-dev
 
-echo "<INFO> Copying vendor libraries to $LBPDATA/$PDIR/$PSHNAME/vendor"
-cp -r ./data/vendor $LBPDATA/$PDIR/$PSHNAME/vendor
+PDATA=$LBPDATA/$PDIR
+TARGETDIR=$PDATA/vendor
+echo "<INFO> Copying vendor libraries to $TARGETDIR"
+if [ -f TARGETDIR ] ; then
+    rm TARGETDIR
+fi
+mv ./data/vendor/* TARGETDIR
 
 # Exit with Status 0
 exit 0
