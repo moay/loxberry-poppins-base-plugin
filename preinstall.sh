4c19a0c6358ae1f20a152a5829a017912aade822
moay <mv@moay.de>
2020-01-04 09:02:34 +0100

    Fixes setup

--- preinstall.sh
+++ preinstall.sh
@@ -6,6 +6,10 @@ TARGETDIR=$PDATA/vendor
 
 echo "<INFO> Preparing vendor libraries"
 php composer.phar install --no-interaction --no-dev
+find ./data/vendor/composer/ -type f -name "*.php" | xargs sed -i "s+'/data/src'+'/src'+g"
+find ./data/vendor/composer/ -type f -name "*.php" | xargs sed -i "s+baseDir = dirname(dirname(+baseDir = dirname(+g"
+
+
 sed -i "s+'/data/src'+'/src'+g" ./data/vendor/composer/autoload_psr4.php
 
 #echo "<INFO> Copying vendor libraries to $TARGETDIR"
