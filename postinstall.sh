ebcb4259669845ae7f3e740f37d89a383e9aa587
moay <mv@moay.de>
2020-01-05 16:58:51 +0100

    Fixes postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-PDIR=$3
-PHTML=$LBPHTML/$PDIR
-PHTMLAUTH=$LBPHTMLAUTH/$PDIR
-
 echo "<INFO> Copying .htaccess files to target location"
-cp ./webfrontend/html/.htaccess $PHTML/.htaccess
-cp ./webfrontend/htmlauth/.htaccess $PHTMLAUTH/.htaccess
+cp ./webfrontend/html/.htaccess REPLACELBPHTMLDIR/.htaccess
+cp ./webfrontend/htmlauth/.htaccess REPLACELBPHTMLAUTHDIR/.htaccess
 
 echo "<OK> Done copying .htaccess files"
 
