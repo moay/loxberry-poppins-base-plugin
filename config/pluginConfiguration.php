fdf64e924999b3c6445160f1293e4eb8c4eae4e7
moay <mv@moay.de>
2020-01-06 14:09:10 +0100

    Reformats directory structure

---
+++ config/pluginConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+
+use LoxBerry\ConfigurationParser\ConfigurationParser;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function(ContainerConfigurator $configurator) {
+    if (!file_exists(__DIR__.'/plugin.cfg')) {
+        throw new \RuntimeException('Cannot load plugin configuration. Configuration file missing.');
+    }
+
+    $configuration = new ConfigurationParser(__DIR__.'/plugin.cfg');
+
+    $configurator->parameters()
+        ->set('plugin.name', $configuration->get('PLUGIN', 'NAME'))
+        ->set('plugin.version', $configuration->get('PLUGIN', 'VERSION'))
+        ->set('plugin.directory', $configuration->get('PLUGIN', 'FOLDER'))
+        ->set('plugin.title', $configuration->get('PLUGIN', 'TITLE'))
+    ;
+};
--- preinstall.sh
+++ preinstall.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 echo "<INFO> Preparing vendor libraries"
-php composer.phar install --no-interaction --no-dev
-find ./data/vendor/composer/ -type f -name "*.php" | xargs sed -i "s+\$baseDir . '/data/+\$baseDir . '/+g"
-find ./data/vendor/composer/ -type f -name "*.php" | xargs sed -i "s+__DIR__ . '/../../..' . '/data/+__DIR__ . '/../..' . '/+g"
-find ./data/vendor/composer/ -type f -name "*.php" | xargs sed -i "s+\$baseDir = dirname(dirname(\$vendorDir))+\$baseDir = dirname(\$vendorDir)+g"
-sed -i "s+'/data/src'+'/src'+g" ./data/vendor/composer/autoload_psr4.php
+php composer.phar install --no-interaction --no-dev --no-progress
 echo "<OK> Done installing vendor libraries"
 
 echo "<INFO> Copying plugin configuration into installation"
-cp ./plugin.cfg ./data/config/plugin.cfg
+cp ./config/plugin.cfg ./plugin.cfg
 echo "<OK> Done copying plugin configuration"
+
+echo "<INFO> Moving all code folders to data"
+cp ./config/plugin.cfg ./plugin.cfg
+mkdir ./data
+find . -type d -maxdepth 1 ! -name "data" ! -name "resources" ! -name . -print -exec mv {} ./data \;
+
+echo "<INFO> Moving all resource folders and files to plugin root"
+find ./resources/* -maxdepth 0 ! -name . ! -name .. -print -exec mv {} ./ \;
+rm -f ./resources
+
+echo "<OK> Done moving files and folders."
+echo "<INFO> Ready for installation."
 
 # Exit with Status 0
 exit 0
